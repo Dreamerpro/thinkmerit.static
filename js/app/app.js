6e13dc1988c74bba0ba20be84b0b3324334d1f1a
dreamerpro <miltonbrahma@gmail.com>
2016-03-30 12:02:52 +0530

    mobile optimize

--- js/app/app.js
+++ js/app/app.js
@@ -1,6 +1,6 @@
 
 angular.module('thinkmerit',['ngRoute','ngCookies','ui.calendar','chart.js','chieffancypants.loadingBar', 'ngAnimate','ui.select', 'ngSanitize','ngIdle'])
-.constant('AP', 'http://api.thinkmerit.in')
+.constant('AP', 'https://api.thinkmerit.in')
 .config(['$routeProvider','$httpProvider','$locationProvider', 
     function($routeProvider, $httpProvider, $locationProvider) {
 
