c090c3be83dc64f071fe0eda334b3cc33ab96a93
Dreamerpro <miltonbrahma@gmail.com>
2016-06-24 12:19:50 +0530

    embed video position

--- js/services/service.js
+++ js/services/service.js
@@ -98,7 +98,7 @@ angular.module('thinkmerit')
 			})
 			$('#question-video-modal').modal({show:true});
 			$("#q-video-container").html(
-				"<iframe width='500' allowfullscreen height='350'src='"+video.url+"' style=\"margin:0 auto\"></iframe>"
+				"<iframe width='500' allowfullscreen height='350'src='"+video.url+"' style=\"display:block;margin:0 auto\"></iframe>"
 			);
 		},
 		togglefavourite:function (_self,question,list) {
