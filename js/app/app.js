3df2695fb1b481af2945ee4c6bdf760c7f7213dc
lookplex <lookplex@gmail.com>
2016-10-31 19:08:49 +0530

    api fix

--- js/app/app.js
+++ js/app/app.js
@@ -1,7 +1,7 @@
 
 angular.module('thinkmerit',['ngRoute','ngCookies','ui.calendar','chart.js','chieffancypants.loadingBar', 'ngAnimate','ui.select', 'ngSanitize','ngIdle','rzModule'])
 .constant('AP', 'https://api.thinkmerit.in/api')
-// .constant('AP', 'http://dev.api.thinkmerit.in')
+// .constant('AP', 'http://dev.api.thinkmerit.in/api')
 .config(['$routeProvider','$httpProvider','$locationProvider',
     function($routeProvider, $httpProvider, $locationProvider) {
 
